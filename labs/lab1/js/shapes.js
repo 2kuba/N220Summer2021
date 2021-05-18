95ac26ecaf0aa4eee199919e143bd117762ade73
2kuba <84041803+2kuba@users.noreply.github.com>
2021-05-17 21:04:32 -0400

    lab1 update

--- labs/lab1/js/shapes.js
+++ labs/lab1/js/shapes.js
@@ -2,12 +2,14 @@
 
 function setup() {
     createCanvas(800,600);
+
+    ellipse(65,65,50,50);
+    fill(blue);
 }
 
 function draw() {
     background(white);
 
-    ellipse(65,65,50,50);
-    fill(blue);
+    
 
 }
