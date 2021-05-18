4f73a93315475ccecd37a5315749bbd0fb3475f0
2kuba <84041803+2kuba@users.noreply.github.com>
2021-05-17 21:36:33 -0400

    lab1 update cont

--- labs/lab1/js/shapes.js
+++ labs/lab1/js/shapes.js
@@ -3,13 +3,36 @@
 function setup() {
     createCanvas(800,600);
 
-    ellipse(65,65,50,50);
-    fill(blue);
-}
+    background(255,255,255);
 
-function draw() {
-    background(white);
+    var clownfish = "#e3b220";
+    var dory = "#1b84cf";
 
+    fill(115, 76, 18);
+    rect(0,350,600,250);
+
+    fill(255,255,255);
+    rect(75, 450, 450, 150);
+
+    fill(66, 221, 245);
+    stroke(60, 66, 61);
+    strokeWeight(10);
+    rect(100, 175, 400, 200);
+
+    fill(63, 105, 63);
+    noStroke();
+    rect(105,305,390,65);
+   
+    fill(clownfish);
+    noStroke();
+    ellipse(250,250,90,40);
+
+    fill(clownfish);
+    noStroke();
+    ellipse(280,300,55,28);
+
+    fill(dory);
+    noStroke();
+    ellipse(400,250,105,80);
     
-
 }
