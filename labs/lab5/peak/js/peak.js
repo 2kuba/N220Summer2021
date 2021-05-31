227e446ec67f0a2c248a95386d01717b227fb72c
2kuba <84041803+2kuba@users.noreply.github.com>
2021-05-31 18:12:17 -0400

    lab5 update

---
+++ labs/lab5/peak/js/peak.js
@@ -0,0 +1,11 @@
+
+
+let dvPixel = document.getElementById("pixel");
+
+dvPixel.style.backgroundColor = "#3fd100";
+dvPixel.style.width = "100px";
+dvPixel.style.height = "100px";
+
+function incSize() {
+    
+}
