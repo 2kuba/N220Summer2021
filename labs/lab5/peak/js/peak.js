794c1c2e3535cfc4d49e90003ec3983eaefe57f9
2kuba <84041803+2kuba@users.noreply.github.com>
2021-05-31 21:45:14 -0400

    lab5 peak section

--- labs/lab5/peak/js/peak.js
+++ labs/lab5/peak/js/peak.js
@@ -1,11 +1,18 @@
 
+var w = 100;
+var h = 100;
 
 let dvPixel = document.getElementById("pixel");
 
-dvPixel.style.backgroundColor = "#3fd100";
-dvPixel.style.width = "100px";
-dvPixel.style.height = "100px";
-
 function incSize() {
+    w = w * 1.1;
+    h = h * 1.1;
     
+    return w, h;
+}
+
+function draw() {
+    dvPixel.style.backgroundColor = "#3fd100";
+    dvPixel.style.width = w + "px";
+    dvPixel.style.height = h + "px";
 }
