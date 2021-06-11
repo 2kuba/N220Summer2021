228978eb07796206d07893b67e13c0963267d7ec
2kuba <84041803+2kuba@users.noreply.github.com>
2021-06-10 20:06:54 -0400

    comments added

--- labs/lab7/tipCalc/js/tipCalc.js
+++ labs/lab7/tipCalc/js/tipCalc.js
@@ -1,11 +1,21 @@
+//retrieve value for bill amount from user input
 let billAmt = document.getElementById("billAmt");
 
+//calculate the tip and total
 function calcTip() {
+    
+    //get value of input as a number
     let bill = Number(billAmt.value);
+
+    //calculate tip
     let tip = Number(bill * 0.2);
+
+    //add tip to bill amount to get total
     let total = Number(bill + tip);
 
+    //create string output for tip and total
     let output = "Tip: $" + tip + ". Total: $" + total;
 
+    //print output to console
     console.log(output);
 }
