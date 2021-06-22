ebe2c60e540f25e1198451c89aafaba041c35407
2kuba <84041803+2kuba@users.noreply.github.com>
2021-06-21 21:19:18 -0400

    lab10 colorChanger comments added

--- labs/lab10/colorChanger/js/colorChanger.js
+++ labs/lab10/colorChanger/js/colorChanger.js
@@ -1,3 +1,4 @@
+//div and attributes for square that turns red
 let square1 = document.getElementById("square1");
 square1.style.backgroundColor = "#737373";
 square1.style.width = "200px";
@@ -5,6 +6,7 @@ square1.style.height = "200px";
 square1.style.float = "left";
 square1.style.margin = "5px";
 
+//div and attributes for square that turns green
 let square2 = document.getElementById("square2");
 square2.style.backgroundColor = "#737373";
 square2.style.width = "200px";
@@ -12,6 +14,7 @@ square2.style.height = "200px";
 square2.style.float = "left";
 square2.style.margin = "5px";
 
+//div and attributes for square that turns blue
 let square3 = document.getElementById("square3");
 square3.style.backgroundColor = "#737373";
 square3.style.width = "200px";
@@ -19,8 +22,10 @@ square3.style.height = "200px";
 square3.style.float = "left";
 square3.style.margin = "5px";
 
+//event listener to process click on div
 element.addEventListener("click", changeColor);
 
+//change the color of the div onclick
 function changeColor(event) {
     let color = event.target.getAttribute("data-color");
 
